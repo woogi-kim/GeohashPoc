bcb88e2631124cdb5569710ada18edf507250932
MinUk <qjvk2880@gmail.com>
2025-11-05 22:13:34 +0900

    Initial Commit

---
+++ src/main/java/com/demo/geohashpoc/GeohashPocApplication.java
@@ -0,0 +1,13 @@
+package com.demo.geohashpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeohashPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeohashPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/geohashpoc/GeohashPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.geohashpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeohashPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
